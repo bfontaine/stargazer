e12bdf17e8619191a66720dc17dfde624cd752a8
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-29 18:02:44 +0200

    whitelist added

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/nlopes/slack"
 	"github.com/vrischmann/envconfig"
@@ -13,6 +15,8 @@ var conf struct {
 
 var api *slack.Client
 var rtm *slack.RTM
+
+var whitelist = make(map[string]struct{})
 
 func main() {
 	if err := envconfig.Init(&conf); err != nil {
@@ -38,6 +42,13 @@ func main() {
 				log.Println("Connecting....")
 			case *slack.ConnectedEvent:
 				log.Println("Connected.")
+			case *slack.MessageEvent:
+				if strings.HasPrefix(ev.Channel, "D") {
+					resp := gotDM(ev.Channel, ev.User, ev.Text)
+					if resp != "" {
+						api.PostMessage(ev.Channel, resp, slackParams)
+					}
+				}
 			default:
 				// skip
 			}
@@ -46,6 +57,11 @@ func main() {
 }
 
 func notify(s Star) {
+	if _, ok := whitelist[s.Message.User]; !ok {
+		log.Printf("%s is not whitelisted, skipping.", s.Message.User)
+		return
+	}
+
 	_, _, ch, err := api.OpenIMChannel(s.Message.User)
 	if err != nil {
 		log.Println(err.Error())
@@ -57,4 +73,28 @@ func notify(s Star) {
 	}
 
 	api.PostMessage(ch, msg, slackParams)
+}
+
+var reEnableMsg = regexp.MustCompile(`\b(enable|yes|oui|want|please)\b`)
+var reDisableMsg = regexp.MustCompile(`\b(disable|no|non|stop|fuck you|go away)\b`)
+
+func gotDM(channel, user, text string) string {
+	if reEnableMsg.MatchString(strings.ToLower(strings.TrimSpace(text))) {
+		if _, ok := whitelist[user]; ok {
+			return "I already know that ;)"
+		}
+
+		whitelist[user] = struct{}{}
+		return "Got it! Happy stalking!"
+
+	} else if reDisableMsg.MatchString(text) {
+		if _, ok := whitelist[user]; !ok {
+			return "I didn’t want to disturb anyway."
+		}
+
+		delete(whitelist, user)
+		return "OK got it. I won't disturb you anymore."
+	}
+
+	return ""
 }
